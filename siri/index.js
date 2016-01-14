554597a849888673982a5ff21578fd763a9cbeed
Phillip Puckett <phillippuckett88@gmail.com>
2016-01-14 10:53:53 -0700

    Siri 4/4

--- siri/index.js
+++ siri/index.js
@@ -3,6 +3,35 @@ var port = 8887;
 
 var app = express();
 
+var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that."];
+
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
+    message: getRandomMessage()
+  }));
+});
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+})
+
 app.listen(port, function (){
    console.log('Listening on port ' + port); 
 });
