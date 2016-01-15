3d6e142890777ed4d41b48ec5b7c7c57f1a92cf7
Phillip Puckett <phillippuckett88@gmail.com>
2016-01-14 23:42:18 -0700

    Chatty 2/5

--- chatty/server.js
+++ chatty/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var bodyParser = ('body-parser');
+var bodyParser = require('body-parser');
+var express = require('express');
 
-var port = 9361;
+var port = 3000;
 
-var app = express()
+var app = express();
+app.use(bodyParser.json());
 
-var messages = [];
+var messages = ["Hello!"];
 
 app.get('/', function (req, res) {
     res.status(200).set({
@@ -19,7 +21,21 @@ app.get('/', function (req, res) {
     }).send(JSON.stringify(messages));
 });
 
+app.post('/', function (req, res) {
 
+    messages.push(req.body.message);
+
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+
+});
 
 app.listen(port, function () {
     console.log("Listening in on, " + port);
